226642a43bc1f791dbd32117ed4923d401b55e67
berta.mgarcia <berta.mgarcia@alumnos.upm.es>
2015-05-06 11:04:53 +0200

    Nuevo cambio

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,14 +1,26 @@
 var models = require('../models/models.js');
+
+//GET /quizes
+exports.index = function(req,res) {
+        models.Quiz.findAll().success(function(quizes){
+       res.render('quizes/index.ejs', {quizes: quizes});
+      })
+};
+
 //GET /quizes/:id
 exports.show = function(req,res) {
         models.Quiz.find(req.params.quizId).then(function(quiz) {
-	 res.render('quizes/show', {pregunta: quiz[0].pregunta})
+       //  models.Quiz.findAll().success(function(quiz){
+	// res.render('quizes/show', {pregunta: quiz[0].pregunta})
+       res.render('quizes/show', {quiz: quiz});
 })
 };
 
 //GET /quizes/:id/answer
 exports.answer = function(req,res) {
-models.Quiz.find(req.params.quizId).then(function(quiz) {
+ //models.Quiz.findAll().success(function(quiz){
+  models.Quiz.find(req.params.quizId).then(function(quiz) {
+          //if (req.query.respuesta === quiz[0].respuesta){
 	if (req.query.respuesta === quiz.respuesta){
 	  res.render('quizes/answer', 
                {quiz:quiz,  respuesta: 'Correcto' });
